3e69ed096ac3efde0d4e4a6f2834751af4ebea78
Rishika Jain <rishika@mediaamp.co.in>
2024-01-31 15:28:42 +0530

    Complete project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Form from "./Form";
-import List from "./List";
+// import List from "./List";
+import Table from "./Table";
 
 function App() {
   const API_URL = "https://jsonplaceholder.typicode.com/"
@@ -29,12 +30,15 @@ function App() {
         setReqType={setReqType} 
       />
 
-      <List 
+      {/* <List 
         items={items}
-      />
+      /> */}
+
+    <Table 
+      items={items}
+    />
 
 
-      
     </div>
   );
 }
